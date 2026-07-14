65a1e1eb79aa057f74fa604a421d0a8ba58cdaf4
Roman Khonyakov <91469344+Razdsgn@users.noreply.github.com>
2026-07-15 00:21:52 +0200

    Update lenis-provider.tsx

--- components/lenis-provider.tsx
+++ components/lenis-provider.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.8,          // уменьшили с 1.2 для отзывчивости
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
+      wheelMultiplier: 1,
     });
 
     function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
 
     return () => {
