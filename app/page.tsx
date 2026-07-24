f15229f051b439ed1174dc0b4adb615c41911144
Roman Khonyakov <91469344+Razdsgn@users.noreply.github.com>
2026-07-24 23:46:11 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ function SectionWrapper({
   children: React.ReactNode;
 }) {
   const ref = useRef(null);
-  const isInView = useInView(ref, { threshold: 0.2 });
+  const isInView = useInView(ref, { amount: 0.2 });
 
   return (
     <motion.section
