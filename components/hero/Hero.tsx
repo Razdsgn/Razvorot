568ccf7a372c4bb1944fd07a383bcdcc81410450
Roman Khonyakov <91469344+Razdsgn@users.noreply.github.com>
2026-07-27 15:11:06 +0200

    Update Hero.tsx

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import Container from "@/components/ui/Container";
 import HeroHeading from "./HeroHeading";
 import HeroMeta from "./HeroMeta";
 import HeroBackground from "./HeroBackground";
+import HeroAnimation from "./HeroAnimation";
 
 import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
+      <HeroAnimation />
+
       <HeroBackground />
 
       <Container>
         <div className={styles.inner}>
           <HeroMeta />
-
           <HeroHeading />
         </div>
       </Container>
