2ea85ba10d50920c0b59e395a715e9cb56b93080
Roman Khonyakov <91469344+Razdsgn@users.noreply.github.com>
2026-07-27 15:03:12 +0200

    Create HeroMeta.tsx

---
+++ components/hero/HeroMeta.tsx
@@ -0,0 +1,25 @@
+import styles from "./Hero.module.css";
+
+export default function HeroMeta() {
+  return (
+    <div className={styles.meta}>
+      <div>
+
+        <span>Rennes</span>
+
+      </div>
+
+      <div>
+
+        <span>France</span>
+
+      </div>
+
+      <div>
+
+        <span>2026</span>
+
+      </div>
+    </div>
+  );
+}
