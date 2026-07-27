eb97df9cc47b08a892b8c9a45e4f5d6f5949e9a0
Roman Khonyakov <91469344+Razdsgn@users.noreply.github.com>
2026-07-27 15:02:42 +0200

    Create HeroHeading.tsx

---
+++ components/hero/HeroHeading.tsx
@@ -0,0 +1,11 @@
+import styles from "./Hero.module.css";
+
+export default function HeroHeading() {
+  return (
+    <h1 className={styles.title}>
+      <span>CREATING</span>
+      <span>DIGITAL</span>
+      <span>SYSTEMS</span>
+    </h1>
+  );
+}
